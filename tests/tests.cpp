0467ba39b32ad97e57b47a9a1182cca4791aa8a5
damon <mcwertgamingQgmail.com>
2021-01-10 01:14:15 +0000

    added first sample test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,3 +5,11 @@ TEST(googleTestWorking, googleTestWorking)
 {
     EXPECT_EQ(true, true);
 }
+
+TEST(illegalInputTesting, notExistingPice)
+{
+    chess::chess chf;
+    chf.clearField();
+    EXPECT_FALSE(chf.canMovePice(2,2,2,3, true)) << "non-existing white piece";
+    EXPECT_FALSE(chf.canMovePice(3,4,5,6, false));
+}
